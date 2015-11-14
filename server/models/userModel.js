4efb606a98651d59037b41112dfc08f191f8698b
Matthew Murkidjanian <matthew>
2015-11-13 19:21:12 -0800

    (feature) construct remaining models and collections This commit tentatively completes construction of bookshelf.js models and collections. In order to establish follower-followee connections, I add the followerUserModel.js file. This is a temporary bridge model that contains just a tableName attribute. This is essentially a second copy of the User model with a different name and extra relations.

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,8 +1,20 @@
 var db = require('../config/schema');
 var Promise = require('bluebird');
 
+require('./wantToTryModel');
+require('./followerUserModel');
+
 var User = db.Model.extend({
-  tableName: 'users'
+  tableName: 'users',
+  wantToTrys: function() {
+    return this.hasMany('WantToTry');
+  },
+  followers: function() {
+    return this.belongsToMany(FollowerUser, 'followers', 'being_followed_id', 'follower_id');
+  },
+  following: function() {
+    return this.belongsToMany(FollowerUser, 'followers', 'follower_id', 'being_followed_id');
+  }
 });
 
 module.exports = db.model('User', User);
