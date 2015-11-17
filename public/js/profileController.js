3d2ee6e0e78deeb76e8dd8325117bc7421919ed7
Kevin Cheng <09chengk@gmail.com>
2015-11-17 12:13:07 -0800

    (feature) adds profileController

--- public/js/profileController.js
+++ public/js/profileController.js
@@ -1,3 +1,25 @@
-tastebuds.controller('profileController', function($scope) {
-  $scope.message = 'lol the profileController works.';
+tastebuds.controller('profileController', function($scope, $http) {
+    $scope.profile = {
+        username: users.username,
+        posts: [],
+        wantToEat: []
+    }
+    $scope.getPosts = $http.get({
+        method: 'GET',
+        url: '/posts'
+    }).then(function(posts) {
+        return posts;
+    })
+    $scope.getEats = $http.get({
+        method: 'GET',
+        // can this url be directed at the getEats row in the posts table?
+        url: '/posts'
+    }).then(function(eat) {
+        wantToEat.push(eat)
+    })
+    $scope.
+    // do the get request in a factory! define your functions, like
+    // var getPosts = function() {
+        // http.get({ posts })}
+    //build a get request that gets posts that matches the username
 });
