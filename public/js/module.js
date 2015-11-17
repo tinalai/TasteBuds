ea89a1e448e50eead28c38dcb9c28cec8ac10578
VDUCHEW <vidiuchiu@gmail.com>
2015-11-17 12:22:52 -0800

    (feature) adjust UI syntax on feedController and index

--- public/js/module.js
+++ public/js/module.js
@@ -1,1 +1,20 @@
-tastebuds = angular.module('tastebuds', ['feedController', 'landingController', 'profileController', 'uploadController']);
+tastebuds = angular.module('tastebuds', [
+  'ui.router',
+  'feedController',
+  'landingController',
+  'profileController',
+  'uploadController'
+]);
+
+.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/');
+  ​
+  $stateProvider
+  .state('post', {
+    url: '/post',
+    templateUrl: 'views/feed.html',
+    controller: 'feedController'
+  });
+
+}]);
+});
